225fd303e42730c691d7dbe65f9f7205bebeb3c3
cruldra <cruldra@gmail.com>
2024-09-16 01:02:12 +0800

    ✨ feat: 添加重试机制和类扩展功能
    - 在 `extensions.kt` 中实现重试功能
    - 在 `cls/extensions.kt` 中添加判断子类的扩展方法

---
+++ core/src/main/kotlin/jzeus/core/cls/extensions.kt
@@ -0,0 +1,7 @@
+package jzeus.core.cls
+
+
+/**
+ * 判断[this]是否为[clazz]的子类或实现类
+ */
+fun Class<*>.isSub(clazz: Class<*>) = clazz.isAssignableFrom(this)
---
+++ core/src/main/kotlin/jzeus/core/retry/extensions.kt
@@ -0,0 +1,35 @@
+package jzeus.core.retry
+
+import jzeus.core.cls.isSub
+import java.time.Duration
+import java.util.concurrent.TimeUnit
+
+/**
+ * 在[任务][task]执行过程中抛出[指定异常][extension]时重试[指定的次数][count]
+ * @param task  Function1 任务
+ * @param count Int 重试次数
+ * @param interval Duration 重试间隔
+ * @param exception  Exception 在任务抛出此异常时才会重试
+ * @return V
+ */
+fun <V> retry(
+    count: Int,
+    interval: Duration,
+    exception: Class<out Exception> = Exception::class.java,
+    task: (index: Int) -> V
+): V? {
+
+    var res: V? = null
+    loop@ for (i in 1..count) {
+        try {
+            res = task(i)
+            break@loop
+        } catch (e: Throwable) {
+            if (e::class.java.isSub(exception)) {
+                if (i == count) throw  e
+            } else throw  e
+        }
+        TimeUnit.SECONDS.sleep(interval.toSeconds())
+    }
+    return res
+}
