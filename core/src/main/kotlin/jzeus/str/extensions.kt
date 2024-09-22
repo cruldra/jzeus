0edf7b76f940b24deddc2eb293b95dff66f47424
cruldra <cruldra@gmail.com>
2024-09-22 18:26:10 +0800

    SZR-40 支持采集作者主页下的所有视频

--- core/src/main/kotlin/jzeus/str/extensions.kt
+++ core/src/main/kotlin/jzeus/str/extensions.kt
@@ -2,6 +2,7 @@ package jzeus.str
 
 import org.apache.commons.exec.CommandLine
 import java.time.format.DateTimeFormatter
+import java.util.regex.Pattern
 
 fun String.toDateTimeFormatter(): DateTimeFormatter {
     return DateTimeFormatter.ofPattern(this)
@@ -18,4 +19,36 @@ fun String.asCommandLine(): CommandLine = CommandLine.parse(this)
  */
 fun StringBuilder.appendWithSpace(str: String) {
     append(" $str")
+}
+
+/**
+ * 判断字符串是否为`URL`
+ */
+val String.isUrl: Boolean
+    get() {
+        val urlPattern = Pattern.compile(
+            "^(https?://)?" + // protocol
+                    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+                    "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+                    "(:\\d+)?(/[-a-z\\d%_.~+]*)*" + // port and path
+                    "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+                    "(#[-a-z\\d_]*)?$", // fragment locator
+            Pattern.CASE_INSENSITIVE
+        )
+        return urlPattern.matcher(this).matches()
+    }
+
+fun main() {
+    val urls = listOf(
+        "https://www.example.com",
+        "http://subdomain.example.co.uk:8080",
+        "https://example.com/path/to/page?name=value&name2=value2",
+        "invalid url",
+        "ftp://example.com",
+        "just.words"
+    )
+
+    urls.forEach { url ->
+        println("$url is ${if (url.isUrl) "valid" else "invalid"}")
+    }
 }
