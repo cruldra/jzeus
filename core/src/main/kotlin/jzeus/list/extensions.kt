e7ef3aeb183e19fa4593657c4f716fcd7ee1a954
cruldra <cruldra@gmail.com>
2024-09-23 17:16:46 +0800

    🎉 feat: 添加 DouyinCrawler 日志记录功能并创建测试运行配置
    ✨ 变更 crawler.kt 中的 DouyinCrawler 类，增加日志记录功能
    ✨ 新增测试运行配置文件用于运行测试代码
    ✨ 在 extensions.kt 中添加获取列表前 n 个元素的文档说明

--- core/src/main/kotlin/jzeus/list/extensions.kt
+++ core/src/main/kotlin/jzeus/list/extensions.kt
@@ -1,5 +1,8 @@
 package jzeus.list
 
+/**
+ * 获取列表的前`n`个元素,如果`n`大于列表长度,则返回整个列表
+ */
 fun <E> List<E>.subList(length: Int): List<E> {
     return if (length <= size) {
         this.subList(0, length)
