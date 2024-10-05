9f769b355be427be16675de8ab8fae8c2a91028c
cruldra <cruldra@gmail.com>
2024-10-05 23:56:06 +0800

    🎉 feat: 删除不再使用的示例代码和类，实现代码清理
    🔧 fix: 更新 BPMN 流程定义，调整任务和事件名称
    
    🌟 feat: 添加新的 JSON 处理库依赖，增强功能
    
    🔄 refactor: 修改类的结构，使其更符合序列化要求
    
    📝 test: 添加对 Camunda 流程的测试用例，确保功能正常
    
    🌐 chore: 更新项目配置，增加必要的配置项和参数
    
    🚀 feat: 实现新的数字人工作流，增强数字人处理能力

--- core/src/main/kotlin/jzeus/any/types.kt
+++ core/src/main/kotlin/jzeus/any/types.kt
@@ -1,6 +1,8 @@
 package jzeus.any
 
-data class Range<T : Comparable<T>>(val min: T, val max: T) {
+import java.io.Serializable
+
+data class Range<T : Comparable<T>>(val min: T, val max: T) : Serializable {
     init {
         require(min <= max) { "Minimum value must be less than or equal to maximum value" }
     }
@@ -11,18 +13,18 @@ data class Range<T : Comparable<T>>(val min: T, val max: T) {
 data class Offset(
     val x: Float = 0.0f,
     val y: Float = 0.0f
-)
+):Serializable
 
 data class Size(
     val width: Int = 0,
     val height: Int = 0,
     val ratio: String? = null
-)
+):Serializable
 
 data class Position(
     val offset: Offset = Offset(),
     val size: Size = Size()
-)
+):Serializable
 
 object Sizes {
     val Douyin = Size(1080, 1920, "9:16")
---
+++ core/src/main/kotlin/jzeus/async/extensions.kt
@@ -0,0 +1,5 @@
+package jzeus.async
+
+import java.util.concurrent.TimeUnit
+
+fun sleep(seconds: Long) = TimeUnit.SECONDS.sleep(seconds)
