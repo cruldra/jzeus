c2d610c50e426e834448571bf03beaa219e4ae53
cruld <cruldra@gmail.com>
2024-11-06 02:37:55 +0800

    ✨ feat(services/audio/openai): 添加OpenAI错误响应处理和Whisper模型支持
    ✨ feat(api): 增加WHISPER_1模型到支持的模型列表
    
    ✨ feat(services/routes): 优化音频转文本服务处理逻辑
    
    ✨ feat(services/extensions): 添加获取支持模型上游服务的功能
    
    ✨ build(docker): 添加Dockerfile和docker-compose配置
    
    ✨ feat(services/vendor/openai): 新增OpenaiErrorResponse数据类
    
    ✨ docs(services/audio/openai/clients): 更新OpenAI音频接口文档说明
    
    ✨ feat(core/http/client): 添加响应内容读取为字符串和文件的转换器

--- core/src/main/kotlin/jzeus/net/http/client/retrofit2/converters.kt
+++ core/src/main/kotlin/jzeus/net/http/client/retrofit2/converters.kt
@@ -10,6 +10,28 @@ import java.lang.reflect.Type
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 
+/**
+ * 这个转换器将响应内容读取为一个字符串
+ *
+ * ```kotlin
+ *
+ * interface Api {
+ *     @GET("/api/v1/str")
+ *     fun getFile(): Call<String>
+ * }
+ *
+ * val api = retrofit {
+ *     baseUrl("https://example.com")
+ *     addConverterFactory(RawStringConverterFactory())
+ * }.create(Api::class.java)
+ *
+ * val content:String = api.getFile().execute().body()
+ * ```
+ * @author dongjak
+ * @created 2024/11/06
+ * @version 1.0
+ * @since 1.0
+ */
 class RawStringConverterFactory : Converter.Factory() {
     private class RawStringConverter : Converter<ResponseBody, String> {
         override fun convert(value: ResponseBody): String {
@@ -29,6 +51,29 @@ class RawStringConverterFactory : Converter.Factory() {
     }
 }
 
+
+/**
+ * 这个转换器将响应内容读取为一个文件
+ *
+ * ```kotlin
+ * interface Api {
+ *     @GET("/api/v1/file")
+ *     fun getFile(): Call<File>
+ * }
+ *
+ * val api = retrofit {
+ *     baseUrl("https://example.com")
+ *     addConverterFactory(FileConverterFactory())
+ * }.create(Api::class.java)
+ *
+ * val file:File = api.getFile().execute().body()
+ * ```
+ *
+ * @author dongjak
+ * @created 2024/11/06
+ * @version 1.0
+ * @since 1.0
+ */
 class FileConverterFactory(private val dir: File = Files.createTempDirectory(uuid()).toFile()) :
     Converter.Factory() {
 
